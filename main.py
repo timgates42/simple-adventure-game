85df4793076ddc1301cdd3c698a23aaf5a319e4a
Snorlax Pokemon <111623284+snorlax-pokemon@users.noreply.github.com>
2022-08-22 17:24:33 +1000

    two rooms

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-print("You are in a dark tunnel.")
-command = input("# ")
-print(f"You try to {command}")
+def dark_tunnel():
+    print("""You are in a dark tunnel.
 
+    Exits are: north
+    """)
+
+def the_field():
+    print("""You are in a green field.
+
+    Exits are: south, east
+    """)
+
+room = dark_tunnel
+show_room = True
+finished = False
+while not finished:
+    if show_room:
+        room()
+        show_room = False
+    command = input("# ")
+    if command == "north":
+        room = the_field
+        show_room = True
+    else:
+        print("Sorry I don't understand.")
