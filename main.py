5e3ef8b028575fb712d15512de01f1a5fb59ea86
Snorlax Pokemon <111623284+snorlax-pokemon@users.noreply.github.com>
2022-08-25 18:07:04 +1000

    heaps of work

--- main.py
+++ main.py
@@ -13,7 +13,25 @@ def the_field():
 def bottom_of_mountain():
     print("""you are next to a huge moutain 
 
-   there is a pathway heading up the mountain east and west
+   there is a pathway heading up the mountain exits are east and west
+    """)
+
+def walking_up_the_mountain():
+    print("""You are up walking up the mountain
+
+    there is a cave (north), keep on walking up the mountain (east) or turn back (west)
+    """)
+
+def cave():
+    print("""You are up walking into a cave and rocks fall behind you 
+
+    the cave has three only one exit and that is north
+    """)
+
+def waterfall():
+    print("""You are up at the top of a mountain and you see a huge waterfall 
+
+    you can walk back (west) or you can walk to the waterfall (east)
     """)
 
 room = dark_tunnel
@@ -34,7 +52,22 @@ while not finished:
         show_room = True     
     elif command == "east" and room == the_field:
         room = bottom_of_mountain 
-        show_room = True     
+        show_room = True  
+     elif command == "west" and room == bottom_of_mountain:
+        room = the_field
+        show_room = True    
+     elif command == "east" and room == bottom_of_mountain:
+        room = walking_up_the_mountain
+        show_room = True  
+    elif command == "west" and room == walking_up_the_mountain:
+        room = bottom_of_mountain
+        show_room = True  
+    elif command == "north" and room == walking_up_the_mountain:
+        room = cave
+        show_room = True  
+    elif command == "east" and room == walking_up_the_mountain:
+        room = waterfall
+        show_room = True  
     else:
         print("Sorry I don't understand.")
     
